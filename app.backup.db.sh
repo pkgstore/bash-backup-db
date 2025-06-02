5a59c95da4a5c87c306434861ac17863f4e4db47
KaiKimera <git@kai.kim>
2025-06-02 14:11:37 +0300

    Update resources

--- app.backup.db.sh
+++ app.backup.db.sh
@@ -68,6 +68,7 @@ function _mongo() {
     "--archive"
     "--db=${db}"
   )
+
   mongodump "${opts[@]}"
 }
 
@@ -81,8 +82,10 @@ function _mysql() {
     "--password=${DB_PASS}"
     "--databases=${db}"
   )
+
   (( "${MYSQL_ST:-1}" )) && opts+=('--single-transaction')
   (( "${MYSQL_SLT:-1}" )) && opts+=('--skip-lock-tables')
+
   "${cmd}" "${opts[@]}"
 }
 
@@ -95,17 +98,28 @@ function _pgsql() {
     '--no-password'
     "--dbname=${db}"
   )
+
   (( "${PGSQL_CL:-1}" )) && opts+=('--clean')
   (( "${PGSQL_IE:-1}" )) && opts+=('--if-exists')
   (( "${PGSQL_NO:-1}" )) && opts+=('--no-owner')
   (( "${PGSQL_NP:-1}" )) && opts+=('--no-privileges')
   (( "${PGSQL_QAI:-1}" )) && opts+=('--quote-all-identifiers')
+
+  case "${PGSQL_FMT:-plain}" in
+    'plain') opts+=('--format=plain') ;;
+    'custom') opts+=('--format=custom') ;;
+    'directory') opts+=('--format=directory') ;;
+    'tar') opts+=('--format=tar') ;;
+    *) _err 'PGSQL_FMT does not exist!' ;;
+  esac
+
   PGPASSWORD="${DB_PASS}" pg_dump "${opts[@]}"
 }
 
 function _dump() {
   local dbms; dbms="${1%%.*}"
   local db; db="${1##*.}"
+
   case "${dbms}" in
     'mongo') _mongo "${db}" ;;
     'mysql') _mysql "${db}" ;;
@@ -117,6 +131,7 @@ function _dump() {
 function _gpg() {
   local out; out="${1}.gpg"
   local pass; pass="${2}"
+
   gpg --batch --passphrase "${pass}" --symmetric --output "${out}" \
     --s2k-cipher-algo "${ENC_GPG_CIPHER:-AES256}" \
     --s2k-digest-algo "${ENC_GPG_DIGEST:-SHA512}" \
@@ -126,6 +141,7 @@ function _gpg() {
 function _ssl() {
   local out; out="${1}.ssl"
   local pass; pass="${2}"
+
   openssl enc "-${ENC_SSL_CIPHER:-aes-256-cfb}" -out "${out}" -pass "pass:${pass}" \
     -salt -md "${ENC_SSL_DIGEST:-sha512}" -iter "${ENC_SSL_COUNT:-65536}" -pbkdf2
 }
@@ -133,6 +149,7 @@ function _ssl() {
 function _enc() {
   local out; out="${1}"
   local pass; pass="${ENC_PASS}"
+
   if (( "${ENC_ON}" )); then
     case "${ENC_APP}" in
       'gpg') _gpg "${out}" "${pass}" ;;
@@ -147,6 +164,7 @@ function _enc() {
 function _sum() {
   local in; in="${1}"; (( "${ENC_ON}" )) && in="${1}.${ENC_APP}"
   local out; out="${in}.txt"
+
   sha256sum "${in}" | sed 's| .*/|  |g' | tee "${out}" > '/dev/null'
 }
 
@@ -163,11 +181,13 @@ function backup() {
 
 function sync() {
   (( ! "${SYNC_ON}" )) && return 0
+
   local opts; opts=('--archive' '--quiet')
   (( "${SYNC_DEL:-0}" )) && opts+=('--delete')
   (( "${SYNC_RSF:-0}" )) && opts+=('--remove-source-files')
   (( "${SYNC_PED:-0}" )) && opts+=('--prune-empty-dirs')
   (( "${SYNC_CVS:-0}" )) && opts+=('--cvs-exclude')
+
   rsync "${opts[@]}" -e "sshpass -p '${SYNC_PASS}' ssh -p ${SYNC_PORT:-22}" \
     "${DB_DST}/" "${SYNC_USER:-root}@${SYNC_HOST}:${SYNC_DST}/"
 }
