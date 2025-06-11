4fbabf63e4b5efc8190de3ea79370f823b2326f3
KaiKimera <git@kai.kim>
2025-06-11 17:48:51 +0300

    Update resources

--- app_backup_db.sh
+++ app_backup_db.sh
@@ -41,7 +41,7 @@ SYNC_DST="${SYNC_DST:?}"; readonly SYNC_DST
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function _err() {
-  echo >&2 "[$( date +'%Y-%m-%dT%H:%M:%S%z' )]: $*"; exit 1
+  echo >&2 "[$( date '+%FT%H:%M:%S%z' )]: $*"; exit 1
 }
 
 function _id() {
@@ -49,7 +49,7 @@ function _id() {
 }
 
 function _timestamp() {
-  date -u '+%Y-%m-%d.%H-%M-%S'
+  date -u '+%F.%H-%M-%S'
 }
 
 function _tree() {
